1b9593d777f708b2f64e38bba6fd2df220b8dfe1
Toon Geens <toon.geens@xenit.eu>
2024-04-29 16:02:25 +0200

    added some javadoc info

--- test/contentgrid-helm-client/src/main/java/com/contentgrid/helm/HelmInstallCommand.java
+++ test/contentgrid-helm-client/src/main/java/com/contentgrid/helm/HelmInstallCommand.java
@@ -30,30 +30,58 @@ public interface HelmInstallCommand {
 
         void apply(InstallOptionsHandler handler);
 
+        /**
+         * Kubernetes namespace scope for this request
+         * @param namespace kubernetes namespace
+         */
         static InstallOption namespace(String namespace) {
             return handler -> handler.namespace(namespace);
         }
 
+        /**
+         * Create the release namespace if not present
+         */
         static InstallOption createNamespace() {
             return InstallOptionsHandler::createNamespace;
         }
 
+        /**
+         * Generate the name (and omit the NAME parameter)
+         */
         static InstallOption generateName() {
             return InstallOptionsHandler::generateName;
         }
 
+        /**
+         * Set values from a values file
+         * @param file is values.yml
+         */
         static InstallOption values(Path file) {
             return handler -> handler.values(file);
         }
 
+        /**
+         * Set JSON values
+         * @param values map with values
+         */
         static InstallOption values(Map<String, Object> values) {
             return handler -> handler.values(values);
         }
 
+        /**
+         * Specify a version constraint for the chart version to use.
+         * This constraint can be a specific tag (e.g. 1.1.1) or it may reference a valid range (e.g. ^2.0.0). If this
+         * is not specified, the latest version is used.
+         * @param version chart version constraint
+         */
         static InstallOption version(String version) {
             return handler -> handler.version(version);
         }
 
+        /**
+         * Time to wait for any individual Kubernetes operation. (default "5m0s")
+         * @param duration timeout expression
+         */
         static InstallOption timeout(String duration) {
             return handler -> handler.timeout(duration);
         }
@@ -61,9 +89,15 @@ public interface HelmInstallCommand {
     }
 
     interface InstallOptionsHandler {
+
+
         void namespace(String namespace);
+
+
         void createNamespace();
+
         void generateName();
+
         void values(Path file);
 
         void values(Map<String, Object> values);
