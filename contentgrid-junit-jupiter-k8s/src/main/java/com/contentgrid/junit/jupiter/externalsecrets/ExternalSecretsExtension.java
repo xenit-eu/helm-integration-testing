cc20d2da15cbffe34c61dee86a719b10f7ff6a4c
Thijs Lemmens <thijs.lemmens@xenit.eu>
2025-02-11 14:15:00 +0100

    ACC-1786 Add tests for external secrets

--- contentgrid-junit-jupiter-k8s/src/main/java/com/contentgrid/junit/jupiter/externalsecrets/ExternalSecretsExtension.java
+++ contentgrid-junit-jupiter-k8s/src/main/java/com/contentgrid/junit/jupiter/externalsecrets/ExternalSecretsExtension.java
@@ -1,27 +1,18 @@
 package com.contentgrid.junit.jupiter.externalsecrets;
 
 import static com.contentgrid.junit.jupiter.helpers.FieldHelper.findFields;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static com.contentgrid.junit.jupiter.k8s.K8sTestUtils.isCiliumNetworkPolicySupported;
+import static com.contentgrid.junit.jupiter.k8s.K8sTestUtils.waitUntilDeploymentsReady;
 import static org.apache.commons.lang3.RandomStringUtils.insecure;
-import static org.awaitility.Awaitility.await;
 
 import com.contentgrid.junit.jupiter.helm.HasHelmClient;
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Strings;
 import io.fabric8.junit.jupiter.HasKubernetesClient;
-import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.awaitility.core.ConditionEvaluationLogger;
-import org.hamcrest.Matchers;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -117,67 +108,5 @@ public class ExternalSecretsExtension implements HasHelmClient, HasKubernetesCli
         return name.substring(0, Math.min(253, name.length())); // Truncate if needed
     }
 
-    private static boolean isCiliumNetworkPolicySupported(KubernetesClient client) {
-        try {
-            // Attempt to get the CRD
-            CustomResourceDefinition crd = client.apiextensions().v1().customResourceDefinitions()
-                    .withName("ciliumnetworkpolicies.cilium.io") // Name of the CiliumNetworkPolicy CRD
-                    .get();
 
-            // If we reach here without an exception, the CRD exists
-            return crd != null;
-
-        } catch (KubernetesClientException e) {
-            // Catch the exception, which indicates the CRD doesn't exist
-            if (e.getCode() == 404) { //check for the 404 code specifically
-                return false;
-            } else {
-                // For other exceptions, it might be something else, rethrow or handle as needed
-                throw e; // Or return false after logging the exception
-            }
-        }
-    }
-
-
-    private void waitUntilDeploymentsReady(int timeout, List<String> deployments, KubernetesClient kubernetesClient) {
-        // wait until expected deployments have available-replica
-        await()
-                .conditionEvaluationListener(new ConditionEvaluationLogger(log::info, SECONDS))
-                .pollInterval(1, SECONDS)
-                .atMost(timeout, SECONDS)
-                .until(() -> deployments.stream()
-                                .map(name -> deployment(name, kubernetesClient))
-                                .filter(deployment -> deployment.availableReplicas() < 1)
-                                .collect(Collectors.toSet()),
-                        Matchers.empty()
-                );
-    }
-
-    @NotNull
-    private NamedDeployment deployment(String name, KubernetesClient kubernetesClient) {
-        return new NamedDeployment(name, kubernetesClient.apps().deployments().withName(name).get());
-    }
-
-    record NamedDeployment(String name, Deployment deployment) {
-
-            boolean isNotFound() {
-                return this.deployment == null;
-            }
-
-            int availableReplicas() {
-                return this.isNotFound()
-                        ? 0 : Objects.requireNonNullElse(this.deployment.getStatus().getAvailableReplicas(), 0);
-            }
-
-            int desiredReplicas() {
-                return this.isNotFound()
-                        ? -1 : Objects.requireNonNullElse(this.deployment.getStatus().getReplicas(), -1);
-            }
-
-            @Override
-            public String toString() {
-                return "%s (%s)".formatted(this.name,
-                        this.isNotFound() ? "MISSING" : this.availableReplicas() + "/" + this.desiredReplicas());
-            }
-        }
 }
--- contentgrid-junit-jupiter-k8s/src/test/java/com/contentgrid/junit/jupiter/externalsecrets/ExternalSecretsExtensionTest.java
+++ contentgrid-junit-jupiter-k8s/src/test/java/com/contentgrid/junit/jupiter/externalsecrets/ExternalSecretsExtensionTest.java
@@ -1,8 +1,14 @@
 package com.contentgrid.junit.jupiter.externalsecrets;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.contentgrid.junit.jupiter.k8s.KubernetesTestCluster;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import java.util.Base64;
+import java.util.Map;
+import java.util.Objects;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -30,10 +36,58 @@ public class ExternalSecretsExtensionTest {
             clusterSecretStoreParameter = parameterClusterSecretStore;
         }
 
+        static KubernetesClient client;
+
         @Test
         void staticInjection() {
             assertEquals("static-secret-store", staticSecretStore.getName());
             assertEquals("static-cluster-secret-store", staticClusterSecretStore.getName());
+
+            var staticKey1 = "staticKey1";
+            var staticValue1 = "staticValue1";
+            staticSecretStore.addSecrets(Map.of(staticKey1, staticValue1
+                    , "staticKey2", "staticValue2"));
+
+            var secretName = "staticExternalSecret".toLowerCase();
+            deployExternalSecret(secretName, "SecretStore", staticSecretStore.getName(), staticKey1, staticKey1);
+            assertSecretValue(secretName, staticKey1, staticValue1);
+
+            var staticClusterKey2 = "staticClusterKey2";
+            var staticClusterValue2 = "staticClusterValue2";
+            staticClusterSecretStore.addSecrets(Map.of("staticClusterKey1", "staticClusterValue1"
+                    , staticClusterKey2, staticClusterValue2));
+            deployExternalSecret(secretName, "ClusterSecretStore", staticClusterSecretStore.getName(), staticClusterKey2, staticClusterKey2);
+            assertSecretValue(secretName, staticClusterKey2, staticClusterValue2);
+        }
+
+        static String externalSecret =
+                    """
+                    apiVersion: external-secrets.io/v1beta1
+                    kind: ExternalSecret
+                    metadata:
+                      name: ${name}
+                    spec:
+                      secretStoreRef:
+                        kind: ${secretStoreKind}
+                        name: ${secretStoreName}
+                      data:
+                        - secretKey: ${secretKey}
+                          remoteRef:
+                            key: ${remoteKey}
+                    """;
+        static void deployExternalSecret(String name, String kind, String secretStoreName, String secretKey, String remoteKey) {
+            client.resourceList(externalSecret
+                    .replace("${name}", name)
+                    .replace("${secretStoreKind}", kind)
+                    .replace("${secretStoreName}", secretStoreName)
+                    .replace("${secretKey}", secretKey)
+                    .replace("${remoteKey}", remoteKey)).serverSideApply();
+        }
+
+        static void assertSecretValue(String secretName, String secretKey, String expectedValue) {
+            var secret = await().atMost(10, SECONDS).until(() -> client.secrets().withName(secretName).get(), Objects::nonNull);
+            var secretValue = new String(Base64.getDecoder().decode(secret.getData().get(secretKey)));
+            assertEquals(expectedValue, secretValue);
         }
 
         @Test
