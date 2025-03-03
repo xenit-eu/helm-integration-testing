cee9a7a8edb95d89d15375f83950af99264ff2da
Thijs Lemmens <thijs.lemmens@xenit.eu>
2025-03-03 15:54:14 +0100

    ACC-1898 Add test method to check that replica's are ready

--- contentgrid-junit-jupiter-k8s/src/main/java/com/contentgrid/junit/jupiter/k8s/K8sTestUtils.java
+++ contentgrid-junit-jupiter-k8s/src/main/java/com/contentgrid/junit/jupiter/k8s/K8sTestUtils.java
@@ -42,46 +42,38 @@ public class K8sTestUtils {
     }
 
 
-    public static void waitUntilDeploymentsReady(int timeout, List<String> deployments, KubernetesClient kubernetesClient) {
+    public static void waitUntilDeploymentsReady(int timeout, List<String> deployments,
+            KubernetesClient kubernetesClient) {
         // wait until expected deployments have available-replica
         await()
                 .conditionEvaluationListener(new ConditionEvaluationLogger(log::info, SECONDS))
                 .pollInterval(1, SECONDS)
                 .atMost(timeout, SECONDS)
                 .until(() -> deployments.stream()
-                                .map(name -> deployment(name, kubernetesClient))
-                                .filter(deployment -> deployment.availableReplicas() < 1)
+                                .map(name -> kubernetesClient.apps().deployments().withName(name).get())
+                                .filter(deployment -> deployment.getStatus().getReplicas() -
+                                        Objects.requireNonNullElse(deployment.getStatus().getReadyReplicas(), 0)
+                                        > 0)
                                 .collect(Collectors.toSet()),
                         Matchers.empty()
                 );
     }
 
-    @NotNull
-    private static NamedDeployment deployment(String name, KubernetesClient kubernetesClient) {
-        return new NamedDeployment(name, kubernetesClient.apps().deployments().withName(name).get());
-    }
-
-    record NamedDeployment(String name, Deployment deployment) {
-
-        boolean isNotFound() {
-            return this.deployment == null;
-        }
-
-        int availableReplicas() {
-            return this.isNotFound()
-                    ? 0 : Objects.requireNonNullElse(this.deployment.getStatus().getAvailableReplicas(), 0);
-        }
-
-        int desiredReplicas() {
-            return this.isNotFound()
-                    ? -1 : Objects.requireNonNullElse(this.deployment.getStatus().getReplicas(), -1);
-        }
-
-        @Override
-        public String toString() {
-            return "%s (%s)".formatted(this.name,
-                    this.isNotFound() ? "MISSING" : this.availableReplicas() + "/" + this.desiredReplicas());
-        }
+    public static void waitUntilReplicaSetsReady(int timeout, List<String> replicaSets,
+            KubernetesClient kubernetesClient) {
+        // wait until expected replicaSets have available-replica
+        await()
+                .conditionEvaluationListener(new ConditionEvaluationLogger(log::info, SECONDS))
+                .pollInterval(1, SECONDS)
+                .atMost(timeout, SECONDS)
+                .until(() -> replicaSets.stream()
+                                .map(name -> kubernetesClient.apps().replicaSets().withName(name).get())
+                                .filter(replicaset -> replicaset.getStatus().getReplicas() -
+                                        Objects.requireNonNullElse(replicaset.getStatus().getReadyReplicas(), 0)
+                                        > 0)
+                                .collect(Collectors.toSet()),
+                        Matchers.empty()
+                );
     }
 
 }
