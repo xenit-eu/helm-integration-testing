a75ecb4413ae9db7bd777cdbdf28cb4f4b757563
Lars Vierbergen <lars.vierbergen@xenit.eu>
2026-09-02 18:09:01 +0200

    Add ability for `ConfigurableResourceSet` to include fabric8 `HasMetadata`
    This allows easily mapping objects applied with
    `client.load(...).create()` (or other output of fabric8) to the resource
    set

--- contentgrid-junit-jupiter-k8s/src/main/java/com/contentgrid/junit/jupiter/k8s/resource/ResourceMatchingSpec.java
+++ contentgrid-junit-jupiter-k8s/src/main/java/com/contentgrid/junit/jupiter/k8s/resource/ResourceMatchingSpec.java
@@ -2,6 +2,7 @@ package com.contentgrid.junit.jupiter.k8s.resource;
 
 import com.contentgrid.helm.HelmInstallCommand.InstallResult;
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import java.util.List;
 import lombok.NonNull;
 
 public interface ResourceMatchingSpec<R extends ResourceMatchingSpec<R>> {
@@ -21,6 +22,12 @@ public interface ResourceMatchingSpec<R extends ResourceMatchingSpec<R>> {
      * @param installResult The helm install result
      */
      R include(@NonNull InstallResult installResult);
+
+    /**
+     * Include all supported resources from fabric8 kubernetes client to the wait
+     * @param items The resources with metadata
+     */
+    R include(@NonNull List<HasMetadata> items);
 
     /**
      * Exclude certain resources from the wait
