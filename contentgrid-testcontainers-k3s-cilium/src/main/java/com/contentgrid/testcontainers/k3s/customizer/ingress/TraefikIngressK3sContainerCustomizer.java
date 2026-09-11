316a9e46a51dff3d37ec79d3ab64cea70dc54db5
Lars Vierbergen <lars.vierbergen@xenit.eu>
2026-09-11 16:26:40 +0200

    Make `TraefikIngressK3sContainerCustomizer#withHelmValues()` private
    It is very likely a mistake when trying to override all helm values
    (including the builtin ones) instead of just adding or removing some
    values.

--- contentgrid-testcontainers-k3s-cilium/src/main/java/com/contentgrid/testcontainers/k3s/customizer/ingress/TraefikIngressK3sContainerCustomizer.java
+++ contentgrid-testcontainers-k3s-cilium/src/main/java/com/contentgrid/testcontainers/k3s/customizer/ingress/TraefikIngressK3sContainerCustomizer.java
@@ -18,6 +18,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.SneakyThrows;
